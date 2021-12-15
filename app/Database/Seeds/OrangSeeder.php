867371b26fabba991baac711cd6ffecc79035f20
bintadiazf <dzbinta11@gmail.com>
2021-12-15 16:00:41 +0700

    14 Desember 2021

--- app/Database/Seeds/OrangSeeder.php
+++ app/Database/Seeds/OrangSeeder.php
@@ -8,26 +8,39 @@ class OrangSeeder extends \CodeIgniter\Database\Seeder
 {
     public function run()
     {
-        $data = [
-            [
-                'nama'       => 'binta',
-                'alamat'     => 'Jl. ABC No. 123',
-                'created_at' => Time::now(),
-                'update_at' => Time::now()
-            ],
-            [
-                'nama'       => 'diaz',
-                'alamat'     => 'Jl. ABC No. 456',
-                'created_at' => Time::now(),
-                'update_at' => Time::now()
-            ],
-            [
-                'nama'       => 'farahita',
-                'alamat'     => 'Jl. DEF No. 001',
-                'created_at' => Time::now(),
-                'update_at' => Time::now()
-            ]
-        ];
+        // $data = [
+        //     [
+        //         'nama'       => 'binta',
+        //         'alamat'     => 'Jl. ABC No. 123',
+        //         'created_at' => Time::now(),
+        //         'update_at' => Time::now()
+        //     ],
+        //     [
+        //         'nama'       => 'diaz',
+        //         'alamat'     => 'Jl. ABC No. 456',
+        //         'created_at' => Time::now(),
+        //         'update_at' => Time::now()
+        //     ],
+        //     [
+        //         'nama'       => 'farahita',
+        //         'alamat'     => 'Jl. DEF No. 001',
+        //         'created_at' => Time::now(),
+        //         'update_at' => Time::now()
+        //     ]
+        // ];
+
+
+
+        $faker = \Faker\Factory::create('id_ID');
+        for ($i = 0; $i < 100; $i++) {
+            $data = [
+                'nama'       => $faker->name,
+                'alamat'     => $faker->address,
+                'created_at' => Time::createFromTimestamp($faker->unixTime()),
+                'update_at'  => Time::now()
+            ];
+            $this->db->table('orang')->insert($data);
+        }
 
         // // Simple Queries
         // $this->db->query(
@@ -36,7 +49,6 @@ class OrangSeeder extends \CodeIgniter\Database\Seeder
         // );
 
         //Using Query Builder
-        // $this->db->table('orang')->insert($data);
-        $this->db->table('orang')->insertBatch($data);
+        // $this->db->table('orang')->insertBatch($data);
     }
 }
